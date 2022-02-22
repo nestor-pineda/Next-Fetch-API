102d8a31b500d984240760fa8c509fb04f0d7482
Nestor Pineda <nestorpinedaelvira@gmail.com>
2022-02-22 21:13:51 +0100

    Some styles added

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div>
+    <footer>
       <p>Copyright 2022 Néstor Pineda</p>
-    </div>
+    </footer>
   );
 };
 
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,12 @@
 const About = () => {
-  return <h1>This is the about page</h1>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+    </div>
+  );
 };
 
 export default About;
